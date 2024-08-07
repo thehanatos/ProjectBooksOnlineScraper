301fb3ee8ff06a909ea49afd7322157140d78d70
thehanatos <admin4523@gmail.com>
2024-08-07 10:02:06 +0200

    phase 2 extract data from the next pages

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+# Extract Data
+
 base_url_img = "https://books.toscrape.com/"
 product_page_url = "https://books.toscrape.com/catalogue/sharp-objects_997/index.html"
 
 
-page = requests.get(product_page_url)
-soup = BeautifulSoup(page.content, "html.parser")
+def get_soup(url):
+    """
+    Fetch the content of the given URL and parse it into a BeautifulSoup object.
+    """
+    page = requests.get(url)
+    page.raise_for_status()  # Raises an error if the request was unsuccessful
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup
 
-# Extract Data
+
+soup = get_soup(product_page_url)
 
 
 def get_title(soup):
@@ -82,51 +91,70 @@ def extract_product_info(soup):
 
 
 # Transform Data
-products_data = []
+book_data = []
 # Extract product info and add to the list
 product_info = extract_product_info(soup)
-products_data.append(product_info)
+book_data.append(product_info)
 # Convert the list of dictionaries into a pandas DataFrame
-df = pandas.DataFrame(products_data)
+df = pandas.DataFrame(book_data)
 # Write the DataFrame to a CSV file
 df.to_csv("product_page_data.csv", index=False)
-print("CSV file has been created.")
+print("CSV file for the book info has been created.")
 
 """
-Phase 2: Extract the relevant data from the selected category page, capturing the following details for each product:
+Phase 2: Extract the relevant data from all the pages of the category, capturing the following details for each product:
 product_page_url, universal_product_code (upc), title, price_including_tax, price_excluding_tax,
 number_available, product_description, category, review_rating, image_url
 After retrieving the data, save it to a CSV file using the above fields as column headers.
 """
-# Phase 2 extract datqa for a category
-# https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html
-# https://books.toscrape.com/catalogue/category/books/fantasy_19/page-2.html
-# https://books.toscrape.com/catalogue/category/books/fantasy_19/page-3.html
+# Phase 2 extract all books data for a category
+
 base_url_book = "https://books.toscrape.com/catalogue/"
 category_books = (
     "https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"
 )
-category_page = requests.get(category_books)
-soup = BeautifulSoup(category_page.content, "html.parser")
-books = soup.find_all("div", class_="image_container")
 
-# Extract all links within these containers
-books_links = []
-for book in books:
-    link = book.find("a")
-    if link and link.has_attr("href"):
-        link = (link["href"])[9:]
-        link = urljoin(base_url_book, link)  # tranform into urls
-        books_links.append(link)
 
+def scrape_books_from_category(category_url):
+    """
+    Scrape all books from a given category page, including all subsequent pages.
+    """
+    books_links = []
+
+    while category_url:
+        # Fetch and parse the current category page
+        soup = get_soup(category_url)
+
+        # Find all book links on the current page
+        books = soup.find_all("div", class_="image_container")
+        for book in books:
+            link = book.find("a")
+            if link and link.has_attr("href"):
+                link = (link["href"])[9:]
+                full_link = urljoin(base_url_book, link)
+                books_links.append(full_link)
+
+        # Check for a "next" page
+        next_button = soup.find("li", class_="next")
+        if next_button:
+            next_link = next_button.find("a")["href"]
+            category_url = urljoin(category_url, next_link)
+        else:
+            category_url = None  # No more pages
+
+    return books_links
+
+
+soup = get_soup(category_books)
+all_books = scrape_books_from_category(category_books)
 
 category_books_data = []
 
-for link in books_links:
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, "html.parser")
+for link in all_books:
+    soup = get_soup(link)
     product_info = extract_product_info(soup)
     category_books_data.append(product_info)
+
 
 df = pandas.DataFrame(category_books_data)
 df.to_csv("category_pages_data.csv", index=False)
